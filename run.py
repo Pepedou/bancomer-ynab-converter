055936a33cddef8c8265267fc3f56b2c47307f54
José Luis Valencia Herrera <jose.valencia@code-ing.com>
2018-10-02 12:53:38 -0500

    Initial commit

---
+++ run.py
@@ -0,0 +1,39 @@
+import csv
+import datetime
+import os
+
+import xlrd
+
+
+def main():
+    os.chdir(os.path.expanduser('~/Desktop'))
+
+    with open('ynab.csv', 'w', encoding='utf8') as f:
+        c = csv.writer(f)
+        c.writerow(['Date', 'Payee', 'Memo', 'Outflow', 'Inflow'])
+
+        with xlrd.open_workbook('descarga.xlsx') as wb:
+            sheet = wb.sheet_by_index(0)
+
+            for i in range(0, sheet.nrows):
+                try:
+                    my_date = datetime.datetime.strptime(sheet.cell_value(i, 0), '%d/%m/%Y')
+                except ValueError:
+                    print('Ignoring row {} "{}", because it does not start with a date.'.format(
+                        i + 1,
+                        sheet.cell_value(i, 0))
+                    )
+                    continue
+
+                row = [
+                    datetime.datetime.strftime(my_date, '%m/%d/%y'),
+                    sheet.cell_value(i, 1),
+                    '_MEMO_',
+                    sheet.cell_value(i, 2),
+                    sheet.cell_value(i, 3) * -1,
+                ]
+                c.writerow(row)
+
+
+if __name__ == '__main__':
+    main()
